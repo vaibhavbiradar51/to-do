0e240bd90409166511672c4c4bf328b75e14d156
vaibhavbiradar51 <48184786+vaibhavbiradar51@users.noreply.github.com>
2021-07-08 03:05:26 +0530

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
     transpileDependencies: ["vuetify", "feathers-vuex"],
     configureWebpack: {
         devServer: {
+          disableHostCheck: true
             proxy: {
                 '/api': {
                     target: 'http://localhost:3000',
